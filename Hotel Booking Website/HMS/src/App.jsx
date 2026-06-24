618b3dc4536a6a577eeb821b9318d6e79a446dac
Nur Bekbolotov <bekbolotovnur09@gmail.com>
2026-06-24 12:42:12 +0600

    Add Rooms section to App component

--- Hotel Booking Website/HMS/src/App.jsx
+++ Hotel Booking Website/HMS/src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./Components/Nav/Nav";
 import Index from "./Components/Pages/Index";
 import Footer from "./Components/Footer/Footer";
 import About from "./Components/Pages/About";
+import Rooms from "./Components/Pages/Rooms";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
+          <Route path="/rooms" element={<Rooms />} />
         </Routes>
         <Footer />
       </BrowserRouter>
